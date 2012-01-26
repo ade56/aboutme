35604a4a0bb51e203e344d84650678e79a1a91a3
Andrew Evans <ade56@uclive.ac.nz>
2012-01-26 22:15:51 +1300

    database options

--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
-$db['default']['database'] = 'blogdb';
+$db['default']['password'] = '9YijdeL4';
+$db['default']['database'] = 'blog';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
