9513b6035c2da7ab787a0fa665fd3a50e3c351f6
Andrew Evans <ade56@uclive.ac.nz>
2012-01-27 15:56:49 +1300

    css changes for info page

---
+++ js/facebook.js
@@ -0,0 +1,20 @@
+<script type="text/javascript">(
+	
+	/** Facebook Like Function at Post-Bottom*/
+	function test(d){
+		 var js, id = 'facebook-jssdk'; 
+		 if (d.getElementById(id)) {return;}
+		 js = d.createElement('script'); 
+		 js.id = id; 
+		 js.async = true;
+		 js.src = "//connect.facebook.net/en_US/all.js#appId=114010215371776&xfbml=1";
+		 d.getElementsByTagName('head')[0].appendChild(js);	 		 
+	}(document));
+		
+	<!-- Place this render call where appropriate -->
+	(function() {
+	    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+	    po.src = 'https://apis.google.com/js/plusone.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+	})();
+</script>
---
+++ js/skype.js
@@ -0,0 +1,6 @@
+<!--
+Skype 'My status' button
+http://www.skype.com/go/skypebuttons
+-->
+<script type="text/javascript" src="http://download.skype.com/share/skypebuttons/js/skypeCheck.js"></script>
+
