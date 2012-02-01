6c7b10874c74cf17fe8c7b62168caff8816951e4
Andrew Evans <ade56@uclive.ac.nz>
2012-02-01 22:58:36 +1300

    CSS Header changes. Banner & Navigation Tabs

--- application/views/shared/menu_view.php
+++ application/views/shared/menu_view.php
@@ -3,7 +3,7 @@
 	<div id='navigation_header'>	
 		<a class='nav_tab home_tab' href='/home/' id='home'>Home</a> 
 		<a class='nav_tab blog_tab' href='/blog/'>Blog</a>
-		<a class='nav_tab info_tab' href='/info/' id='CV_tab'>Information</a> 	 
+		<a class='nav_tab info_tab' href='/info/' id='CV_tab'>Info</a> 	 
 	</div>
 </div>
 
