be72a533c4d00b1aa4b1ee4778c0b1fc5ef83719
Andrew Evans <ade56@uclive.ac.nz>
2012-02-06 17:39:58 +1300

    image, header, css update

--- application/views/shared/menu_view.php
+++ application/views/shared/menu_view.php
@@ -1,5 +1,5 @@
 <div id='header'>
-	<img src='/./images/test2.jpg' alt='Andrew Evans'/>
+	<img src='/./images/test2.jpg' alt='Andrew Evans' style='background:white;'/>
 	<div id='navigation_header'>	
 		<a class='nav_tab home_tab' href='/home/' id='home'>Home</a> 
 		<a class='nav_tab blog_tab' href='/blog/'>Blog</a>
